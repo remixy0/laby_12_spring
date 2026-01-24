3ea5d87964fb94b17997ee5110aad5e2d5fd7837
Stanislaw Sikora <remixy0@onet.pl>
2026-01-24 15:43:49 +0100

    IMU calcualtions

--- src/main/java/org/example/App/Service/IMU.java
+++ src/main/java/org/example/App/Service/IMU.java
@@ -14,24 +14,50 @@ public class IMU implements Sensor{
         period = 1/ effort.getDataRate();
     }
 
-
     @Override
     public double averageSpeed() {
-        return 0;
+        double sum = calculateSpeed().stream().mapToDouble(Double::doubleValue).sum();
+        return sum/calculateSpeed().size();
     }
 
     @Override
     public double averageAcceleration() {
-        return 0;
+        double sum = acceleration.stream().mapToDouble(Double::doubleValue).sum();
+        return sum/acceleration.size();
     }
 
     @Override
     public double timeOfMovement() {
-        return 0;
+        return (finishIndex() - startIndex())*period;
     }
 
     @Override
     public String toString() {
         return effort.getAthleteName() + " " + effort.getAthleteSurname() + "  sensor type: "+  effort.getSensorType() + "     data:" + effort.getData();
+    }
+
+
+    private int startIndex(){
+        for(int i=0; i<calculateSpeed().size(); i++){
+            if(calculateSpeed().get(i) > 1) return i;
+        }
+        return -1;
+    }
+
+    private int finishIndex(){
+        for(int i=startIndex(); i<calculateSpeed().size(); i++){
+            if(calculateSpeed().get(i) < 0.1) return i;
+        }
+        return -1;
+    }
+
+    private List<Double> calculateSpeed() {
+        List<Double> speed = new ArrayList<>();
+        double actualSpeed = 0;
+        for(Double d : acceleration){
+            actualSpeed += d * period;
+            speed.add(actualSpeed);
+        }
+        return speed;
     }
 }
